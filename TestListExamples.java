31eb20da6a82d4b6df7e93b86cb704b7b7c29f02
Scott Semtner <sjsemtner@gmail.com>
2024-02-15 14:53:57 -0800

    epic grader

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,21 +1,128 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
+import org.junit.rules.Timeout;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class IsMoon implements StringChecker {
-  public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
-  }
+    public boolean checkString(String s) {
+        return s.equalsIgnoreCase("moon");
+    }
+}
+
+class CheckContains implements StringChecker {
+    private String contains;
+
+    CheckContains(String contains) {
+        this.contains = contains;
+    }
+
+    @Override
+    public boolean checkString(String s) {
+        return s.contains(contains);
+    }
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
+    @Rule
+    public Timeout timeout =
+            Timeout.seconds(1); // 1 second max per method tested
+
+    @Test
+    public void testMergeRightEnd() {
+        List<String> left = Arrays.asList("a", "b", "c");
+        List<String> right = Arrays.asList("a", "d");
+        List<String> merged = ListExamples.merge(left, right);
+        List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+        assertEquals(expected, merged);
+    }
+
+    @Test
+    public void testFilter() {
+        ArrayList<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("b");
+        input1.add("ab");
+        input1.add("bc");
+        input1.add("abc");
+
+        assertArrayEquals(
+                new String[] {"a", "ab", "abc"},
+                ListExamples.filter(input1, new CheckContains("a")).toArray());
+
+        ArrayList<String> input2 = new ArrayList<>();
+        input2.add("no");
+        input2.add("yes");
+        input2.add("help");
+        input2.add("ahhhh");
+        input2.add("hello");
+
+        assertArrayEquals(
+                new String[] {"help", "hello"},
+                ListExamples.filter(input2, new CheckContains("he")).toArray());
+    }
+
+    @Test
+    public void testFilterEmpty() {
+        ArrayList<String> input = new ArrayList<>();
+        input.add("a");
+        input.add("b");
+        input.add("ab");
+        input.add("bc");
+        input.add("abc");
+
+        assertArrayEquals(
+                new String[] {},
+                ListExamples.filter(input, new IsMoon()).toArray());
+    }
+
+    @Test
+    public void testMergeLeftEnd() {
+        ArrayList<String> input1 = new ArrayList<>();
+        input1.add("apple");
+        input1.add("orange");
+        input1.add("strawberry");
+
+        ArrayList<String> input2 = new ArrayList<>();
+        input2.add("attack");
+        input2.add("battle");
+        input2.add("locate");
+        input2.add("yonder");
+
+        assertArrayEquals(
+                new String[] {
+                    "apple",
+                    "attack",
+                    "battle",
+                    "locate",
+                    "orange",
+                    "strawberry",
+                    "yonder"
+                },
+                ListExamples.merge(input1, input2).toArray());
+    }
+
+    @Test
+    public void testMergeObjects() {
+      ArrayList<String> input1 = new ArrayList<>();
+      var item1 = new String("a");
+      var item2 = new String("b");
+      input1.add(item1);
+      input1.add(item2);
+
+      ArrayList<String> input2 = new ArrayList<>();
+      var item3 = new String("c");
+      var item4 = new String("d");
+      input2.add(item3);
+      input2.add(item4);
+
+      var output = ListExamples.merge(input1, input2);
+      assertSame(item1, output.get(0));
+      assertSame(item2, output.get(1));
+      assertSame(item3, output.get(2));
+      assertSame(item4, output.get(3));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -5,12 +5,74 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone $1 student-submission 2> "grading-area/clone-output.txt"
 
+if [[ $? -ne 0 ]]
+then
+    echo "============================================================"
+    cat clone-output.txt
+    echo "============================================================"
+    echo "Failed to clone repository"
+    echo "Score: 0%"
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+echo "Successfully cloned repository"
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+if ! [[ -f student-submission/ListExamples.java ]]
+then
+    echo "ListExamples.java not found"
+    echo "Score: 0%"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+
+cd grading-area
+
+CLASSPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+
+javac -cp $CLASSPATH *.java > "compile-output.txt" 2>&1
+
+if [[ $? -ne 0 ]]
+then
+    echo "============================================================"
+    cat compile-output.txt
+    echo "============================================================"
+    echo "Failed to compile, see error above"
+    echo "Score: 0%"
+    exit 1
+fi
+
+echo "Compiled successfully"
+echo "Running tests"
+
+java -cp $CLASSPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+
+LAST_LINE=$(tail -n2 test-output.txt | head -n1)
+
+echo $LAST_LINE | grep "OK" -q
+
+if [[ $? -eq 0 ]]
+then
+    TESTS=$(echo $LAST_LINE | cut -d' ' -f 2 | cut -d'(' -f 2)
+    echo "All $TESTS test(s) passed"
+    echo "Score: 100%"
+    exit 0
+fi
+
+TESTS=$(echo $LAST_LINE | cut -d' ' -f 3 | cut -d',' -f 1)
+FAILED=$(echo $LAST_LINE | cut -d' ' -f 5)
+PASSED=$((TESTS - FAILED))
+SCORE=$(echo "scale=1 ; $((PASSED * 100)) / $TESTS" | bc)
+
+# print out failed tests
+echo "Failed test(s) output:"
+echo "============================================================"
+cat test-output.txt | grep -A1 "[0-9]) test"
+echo "============================================================"
+
+echo "Passed $PASSED/$TESTS tests"
+echo "Score: $SCORE%"
+
